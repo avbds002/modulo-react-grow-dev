37366113385d449735cbd345c5d55a373295f5e0
avbds002 <avbds2077@gmail.com>
2026-02-10 19:52:38 -0300

    Introducao SPA e roteamento de paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,6 @@
-import type { ChangeEvent, FormEvent } from "react";
-import { Footer } from "./components/Footer";
-import { Header } from "./components/Header";
-import { Post } from "./components/Post";
-import { posts } from "./posts";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
 
 export function App() {
-  let filterText = "";
-  function handleFilterChange(event: ChangeEvent<HTMLInputElement>) {
-    filterText = event.target.value.toLowerCase();
-    console.log("Texto do filtro: ", filterText);
-  }
-
-  function handleFormSubmit(event: FormEvent) {
-    event.preventDefault();
-
-    const filteredPosts = posts.filter((post) =>
-      post.title?.toLowerCase().includes(filterText),
-    );
-
-    console.log("Posts filtrados: ", filteredPosts);
-  }
-
-  return (
-    <>
-      <Header />
-
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="Filtrar posts por titulo"
-          onChange={handleFilterChange}
-        />
-        <button>Filtrar</button>
-      </form>
-
-      <main>
-        {posts.map((post) => (
-          <Post
-            key={post.id}
-            id={post.id}
-            title={post.title}
-            image={post.image}
-            category={post.category}
-            avatar={post.avatar}
-            createdAt={post.createdAt}
-            description={post.description}
-            author={post.author}
-          ></Post>
-        ))}
-      </main>
-
-      <Footer />
-    </>
-  );
+  return <RouterProvider router={routes} />;
 }
